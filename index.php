f3517d67436ca8a1cffea4da559cf0b449558934
Kiboki <killian.vdw@live.nl>
2016-03-23 08:27:42 +0000

    second

---
+++ index.php
@@ -0,0 +1,14 @@
+<html><body>
+    <form action="connectie.php" method="post">
+        <p>
+            <label for="naam">Naam:</label>
+            <input type="text" name="naam" id="naam">
+        </p>
+        <p>
+            <label for="email">Email:</label>
+            <input type="text" name="email" id="email">
+        </p>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
